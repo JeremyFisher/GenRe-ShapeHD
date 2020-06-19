b8230ec4836e068fdf89a0d4858943d56f638b27
Stefan Stojanov <sstojanov@users.noreply.github.com>
2020-06-19 16:28:39 -0400

    Create download.sh

---
+++ download.sh
@@ -0,0 +1,36 @@
+ #!/usr/bin/env bash
+
+ l1='https://www.dropbox.com/s/w7m01bi3x399gkv/ShapeNet13_2DOFVC_LRBg_GenRe.partaa'
+ l2='https://www.dropbox.com/s/p5jdzxzhk8rajyf/ShapeNet13_2DOFVC_LRBg_GenRe.partab'
+ l3='https://www.dropbox.com/s/9emri8v743chmrv/ShapeNet13_2DOFVC_LRBg_GenRe.partac'
+ l4='https://www.dropbox.com/s/2sp44uag6ujv4sv/ShapeNet13_2DOFVC_LRBg_GenRe.partad'
+ l5='https://www.dropbox.com/s/vuc31l4796qyf68/ShapeNet13_2DOFVC_LRBg_GenRe.partae'
+ l6='https://www.dropbox.com/s/qtv6wx5p9cf2ev6/ShapeNet13_2DOFVC_LRBg_GenRe.partaf'
+ l7='https://www.dropbox.com/s/tq40fqtc085lm43/ShapeNet13_2DOFVC_LRBg_GenRe.partag'
+ l8='https://www.dropbox.com/s/lr7j5vbyqez8lck/ShapeNet13_2DOFVC_LRBg_GenRe.partah'
+ l9='https://www.dropbox.com/s/gd3u7245o7uau5d/ShapeNet13_2DOFVC_LRBg_GenRe.partai'
+ l10='https://www.dropbox.com/s/snhtu3kpe2k5b48/ShapeNet13_2DOFVC_LRBg_GenRe.partaj'
+ l12='https://www.dropbox.com/s/mqetwsnja97m3em/ShapeNet13_2DOFVC_LRBg_GenRe.partak'
+ l13='https://www.dropbox.com/s/v7rkgjzo6evlpj8/ShapeNet13_2DOFVC_LRBg_GenRe.partal'
+ l14='https://www.dropbox.com/s/dax3eij3pch02d1/ShapeNet13_2DOFVC_LRBg_GenRe.partam'
+ l15='https://www.dropbox.com/s/jfm9xmgxcu0njge/ShapeNet13_2DOFVC_LRBg_GenRe.partan'
+ l16='https://www.dropbox.com/s/qwkkp0wlu1kno3p/ShapeNet13_2DOFVC_LRBg_GenRe.partao'
+ l17='https://www.dropbox.com/s/17afuzfbyhf7xnj/ShapeNet13_2DOFVC_LRBg_GenRe.partap'
+
+ wget "$l1";
+ wget "$l2";
+ wget "$l3";
+ wget "$l4";
+ wget "$l5";
+ wget "$l6";
+ wget "$l7";
+ wget "$l8";
+ wget "$l9";
+ wget "$l10";
+ wget "$l11";
+ wget "$l12";
+ wget "$l13";
+ wget "$l14";
+ wget "$l15";
+ wget "$l16";
+ wget "$l17";
